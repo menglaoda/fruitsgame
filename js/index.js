77da41ffc354e91a2a9b747c9a353129948bc677
menglaoda <1415940875@qq.com>
2020-07-02 11:25:54 +0800

    测试图片加载完毕后

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+	//游戏启动中
+	var progresstime = 0;
+	var startgame = setInterval(function(){
+		progresstime++;
+		$(".percent").html(progresstime+"%");
+		$(".loadding").css("width",progresstime+"%");
+		if(progresstime==99){
+			clearInterval(startgame);
+		}
+	},50);
+	//图片加载完毕时，游戏启动成功
+	var imgnum = $("img").length;			
+	$('img').load(function(){
+	    imgnum--;
+	    console.log(imgnum)
+	    if(imgnum==0){
+	    	clearInterval(startgame);
+	    	$(".percent").html("100%");
+			$(".loadding").css("width","100%");
+	    }
+	})
+})
